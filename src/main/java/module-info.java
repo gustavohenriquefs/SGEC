7a1572965cfc7b757cfe9bf5314c3e5678470016
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-09-02 21:17:02 -0300

    adicao de java.sql em module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.casaculturaqxd.sgec {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    
     opens com.casaculturaqxd.sgec to javafx.fxml;
     exports com.casaculturaqxd.sgec;
 }
