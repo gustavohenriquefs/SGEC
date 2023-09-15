f9ad3c5d86bb52dbaab3e022c0dc86b888314b3e
Robson <robsonad07@gmail.com>
2023-09-14 22:55:46 -0300

    metodo conectar implementado na Classe CenectionFactory

--- src/main/java/com/casaculturaqxd/sgec/jdbc/ConnectionFactory.java
+++ src/main/java/com/casaculturaqxd/sgec/jdbc/ConnectionFactory.java
@@ -1,8 +1,22 @@
 package com.casaculturaqxd.sgec.jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConnectionFactory {
     private Connection connection;
 
+    public Connection conectar() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1/javafxmvc", "postgres","493519");
+            return this.connection;
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DatabasePostgreSQL.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 }
