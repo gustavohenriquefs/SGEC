33a06e2e064a15787d571dec25e6df5c810ba91f
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-09-14 23:18:39 -0300

    adicao de teste para configurar o github actions

---
+++ src/test/java/TestGithubActions.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestGithubActions {
+    @Test
+    void TestDummy(){
+        assertEquals(0,0);
+    } 
+
+}
