ec038c3b2c00f9c7402939cb77f75432c21e08b1
Robson <robsonad07@gmail.com>
2023-09-14 22:57:21 -0300

    metodo desconectar implementado na Classe ConectionFactory

--- src/main/java/com/casaculturaqxd/sgec/jdbc/ConnectionFactory.java
+++ src/main/java/com/casaculturaqxd/sgec/jdbc/ConnectionFactory.java
@@ -19,4 +19,12 @@ public class ConnectionFactory {
             return null;
         }
     }
+    
+    public void desconectar(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabasePostgreSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
