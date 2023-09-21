c7b14e6079a9657eae8e97306ad7c4214219f7a8
Robson <robsonad07@gmail.com>
2023-09-21 15:24:14 -0300

    Atributo urlDataBase adicionado

--- src/main/java/com/casaculturaqxd/sgec/jdbc/ConnectionFactory.java
+++ src/main/java/com/casaculturaqxd/sgec/jdbc/ConnectionFactory.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 
 public class ConnectionFactory {
     private Connection connection;
+    private String urlDataBase;
 
     public Connection conectar() {
         try {
