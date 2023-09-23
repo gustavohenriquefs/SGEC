7bf2bed47ae98813f6578b98a80656d8df856b36
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-09-23 18:40:09 -0300

    adicao de 'transitive' nos pacotes do javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module com.casaculturaqxd.sgec {
-    requires javafx.controls;
-    requires javafx.fxml;
+    exports com.casaculturaqxd.sgec.models;
+    exports com.casaculturaqxd.sgec;
+    exports com.casaculturaqxd.sgec.jdbc;
+
+    requires java.logging;
     requires java.sql;
+    requires transitive javafx.base;
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+    requires transitive javafx.graphics;
     
     opens com.casaculturaqxd.sgec to javafx.fxml;
-    exports com.casaculturaqxd.sgec;
+
 }
