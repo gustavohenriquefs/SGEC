e5082c3abc04b2f460024c76df0190e3c98c1c6c
Robson <robsonad07@gmail.com>
2023-09-23 19:11:28 -0300

    Metodo inserir implementado

--- src/main/java/com/casaculturaqxd/sgec/DAO/UserDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/UserDAO.java
@@ -1,6 +1,8 @@
 package com.casaculturaqxd.sgec.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import com.casaculturaqxd.sgec.jdbc.ConnectionFactory;
 import com.casaculturaqxd.sgec.models.User;
@@ -20,8 +22,23 @@ public class UserDAO {
     this.connection = connection;
   }
 
-  public boolean inserir(User user){
-  
+  public boolean inserir(User obj){
+    try {
+      //1° passo - criar comando sql
+      String sql = "insert into tb_clientes (nome,email,senha)"
+              + " values(?,?,?)";
+      //2° passo - conectar o banco de dados e organizar o comando sql
+      PreparedStatement stmt = connection.prepareStatement(sql);
+      stmt.setString(1, obj.getNome_usuario());
+      stmt.setString(2, obj.getEmail());
+      stmt.setString(3, obj.getSenha());
+      //3° passo - executar o comando sql
+      stmt.execute();
+      stmt.close();
+
+    } catch (SQLException e) {
+
+    }
     return true;
   }
 
