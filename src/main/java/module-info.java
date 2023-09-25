d1e7002d5ba9f0d70429da483a5249f79ea6d489
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-09-25 16:34:28 -0300

    adicao do pacote controller a module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.casaculturaqxd.sgec {
     requires javafx.fxml;
     requires java.sql;
     
-    opens com.casaculturaqxd.sgec to javafx.fxml;
+    opens com.casaculturaqxd.sgec.controller to javafx.fxml;
     exports com.casaculturaqxd.sgec;
     requires io.github.cdimascio.dotenv.java;
 }
