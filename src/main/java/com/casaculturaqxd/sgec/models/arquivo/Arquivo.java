d6c7b1444609a6f63a60c78d0ea9715a4caeb349
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-09-25 19:57:05 -0300

    Adicionados atributos faltantes

--- src/main/java/com/casaculturaqxd/sgec/models/arquivo/Arquivo.java
+++ src/main/java/com/casaculturaqxd/sgec/models/arquivo/Arquivo.java
@@ -3,9 +3,20 @@ package com.casaculturaqxd.sgec.models.arquivo;
 import java.io.ByteArrayInputStream;
 
 public abstract class Arquivo {
+    private int idArquivo;
     private String nome;
+    private boolean privado;
     private ByteArrayInputStream imagemPreview;
-
+    private int idEvento;
+    
+    
+    public Arquivo(int idArquivo, String nome, boolean privado, ByteArrayInputStream imagemPreview, int idEvento) {
+        this.idArquivo = idArquivo;
+        this.nome = nome;
+        this.privado = privado;
+        this.imagemPreview = imagemPreview;
+        this.idEvento = idEvento;
+    }
     
     public String getNome() {
         return nome;
@@ -19,6 +30,24 @@ public abstract class Arquivo {
     public void setImagemPreview(ByteArrayInputStream imagemPreview) {
         this.imagemPreview = imagemPreview;
     }
+    public int getIdArquivo() {
+        return idArquivo;
+    }
+    public void setIdArquivo(int idArquivo) {
+        this.idArquivo = idArquivo;
+    }
+    public boolean isPrivado() {
+        return privado;
+    }
+    public void setPrivado(boolean privado) {
+        this.privado = privado;
+    }
 
-
+    public int getIdEvento() {
+        return idEvento;
+    }
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
+    }
+    
 }
