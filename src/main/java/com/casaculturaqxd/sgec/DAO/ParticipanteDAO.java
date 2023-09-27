0fd0ce715b8b18a234a0a29df58d3b6a01d224ec
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2023-09-27 19:03:15 -0300

    organizando imports

--- src/main/java/com/casaculturaqxd/sgec/DAO/ParticipanteDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/ParticipanteDAO.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
-import java.io.ByteArrayInputStream;
 
 import com.casaculturaqxd.sgec.models.Participante;
 
