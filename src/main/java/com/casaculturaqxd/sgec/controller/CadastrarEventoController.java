8410c1711bc0b30402303b7b1fa222e03fb2baaa
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-09-28 10:54:23 -0300

    Adicionando botoes ao controller

--- src/main/java/com/casaculturaqxd/sgec/controller/CadastrarEventoController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/CadastrarEventoController.java
@@ -2,9 +2,12 @@ package com.casaculturaqxd.sgec.controller;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.GridPane;
 
 import com.casaculturaqxd.sgec.models.Localizacao;
 
@@ -20,21 +23,31 @@ public class CadastrarEventoController {
     @FXML
     ChoiceBox<String> classificacaoEtaria;
     private String[] classificacoes = {"Livre","10 anos","12 anos","14 anos","16 anos","18 anos"};
-    /* 
     @FXML
+    CheckBox checkMeta1;
+    @FXML
+    CheckBox checkMeta2;
+    @FXML
+    CheckBox checkMeta3;
+    @FXML
+    CheckBox checkMeta4;
 
+    //Botoes
     @FXML
+    Button adicionarLocalizacao;
+    @FXML
+    Button botaoOrganizadores;
+    @FXML
+    Button botaoColaboradores;
+    @FXML
+    Button botaoParticipantes;
+    @FXML
+    Button botaoArquivos;
+    @FXML
+    Button cancelar;
+    @FXML
+    Button criarEvento;
 
-    @FXML
-
-    @FXML
-
-    @FXML
-
-    @FXML
-
-    @FXML
-    */
     ObservableList<Localizacao> locais;
     public TextFormatter<String> getNumericalFormatter(){
         return new TextFormatter<>(change -> {
@@ -62,6 +75,7 @@ public class CadastrarEventoController {
     
 
     public void initialize(){
+        /* aplicando restrições aos inputs */
         classificacaoEtaria.getItems().addAll(classificacoes);
         publicoEsperado.setTextFormatter(getNumericalFormatter());
         publicoAlcancado.setTextFormatter(getNumericalFormatter());
