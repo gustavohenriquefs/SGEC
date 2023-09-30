47e5fd461e1b8bd34a38cce07706aa4656af3c09
Robson <robsonad07@gmail.com>
2023-09-29 21:33:15 -0300

    metodo getLocalizacao implementado

--- src/main/java/com/casaculturaqxd/sgec/DAO/LocalizacaoDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/LocalizacaoDAO.java
@@ -1,6 +1,8 @@
 package com.casaculturaqxd.sgec.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.casaculturaqxd.sgec.models.Localizacao;
 
@@ -11,7 +13,28 @@ public class LocalizacaoDAO {
     this.connection = connection;
   }
 
-  public Localizacao getLocalizacao(Localizacao localizacao){
-    return null;
+  public Localizacao getLocalizacao(Localizacao obj){
+    String sql = "SELECT * FROM localizacao WHERE idLocalizacao=?";
+    try {
+      PreparedStatement stmt = connection.prepareStatement(sql);
+      stmt.setInt(1, obj.getIdLocalizacao());
+      System.out.println(stmt);
+      ResultSet resultado = stmt.executeQuery();
+      if (resultado.next()) {
+        obj.setIdLocalizacao(resultado.getInt("id_localizacao"));
+        obj.setRua(resultado.getString("rua"));
+        obj.setNumeroRua(resultado.getInt("numero_rua"));
+        obj.setBairro(resultado.getString("bairro"));
+        obj.setCep(resultado.getString("cep"));
+        obj.setCidade(resultado.getString("cidade"));
+        obj.setEstado(resultado.getString("estado"));
+        obj.setPais(resultado.getString("pais"));
+        return obj;
+      } else {
+        return null;
+      }
+    } catch (Exception e) {
+      return null;
+    }
   }
 }
