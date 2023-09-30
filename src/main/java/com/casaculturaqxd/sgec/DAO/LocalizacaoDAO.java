d1702f543df41bbbd725ff91b8e47a704365cb78
Robson <robsonad07@gmail.com>
2023-09-29 22:07:47 -0300

    espaço desnecessário apagado

--- src/main/java/com/casaculturaqxd/sgec/DAO/LocalizacaoDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/LocalizacaoDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import com.casaculturaqxd.sgec.models.Localizacao;
 
 public class LocalizacaoDAO {
