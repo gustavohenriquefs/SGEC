feaf7b702ec0a9cb389a894b54bb591b065b8403
Robson <robsonad07@gmail.com>
2023-09-29 21:21:18 -0300

    metodo setConnection criado

--- src/main/java/com/casaculturaqxd/sgec/DAO/LocalizacaoDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/LocalizacaoDAO.java
@@ -4,4 +4,8 @@ import java.sql.Connection;
 
 public class LocalizacaoDAO {
   private Connection connection;
+
+  public void setConnection(Connection connection) {
+    this.connection = connection;
+  }
 }
