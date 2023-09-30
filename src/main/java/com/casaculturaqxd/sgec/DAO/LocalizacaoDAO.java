6c66c89426ce63466b56cf3c7b9084c433a16cd7
Robson <robsonad07@gmail.com>
2023-09-29 21:25:22 -0300

    Metodo getLocalizacao criado

--- src/main/java/com/casaculturaqxd/sgec/DAO/LocalizacaoDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/LocalizacaoDAO.java
@@ -2,10 +2,16 @@ package com.casaculturaqxd.sgec.DAO;
 
 import java.sql.Connection;
 
+import com.casaculturaqxd.sgec.models.Localizacao;
+
 public class LocalizacaoDAO {
   private Connection connection;
 
   public void setConnection(Connection connection) {
     this.connection = connection;
+  }
+
+  public void getLocalizacao(Localizacao localizacao){
+    
   }
 }
