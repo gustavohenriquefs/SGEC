102331c1227ecc26a7d74976d2806ce04e6ad1a3
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2023-10-01 20:46:29 -0300

    metodo para vincular localizacoes em um evento

--- src/main/java/com/casaculturaqxd/sgec/DAO/EventoDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/EventoDAO.java
@@ -12,4 +12,30 @@ public class EventoDAO {
     this.connection = connection;
   }
 
+  private boolean vincularLocais(SortedSet<Integer> locais) {
+    for(Integer local: locais) {
+      if(!this.vincularLocal(local)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  
+  private boolean vincularLocal(Integer local) {
+    String vincLocaisSql = "INSERT INTO localizacao_evento(id_localizacao, id_evento) VALUES (?, ?)";
+
+    try {
+      PreparedStatement stmt = connection.prepareStatement(vincLocaisSql);
+      stmt.setInt(1, local);
+      stmt.setInt(2, 1);
+      stmt.execute();
+      stmt.close();
+    } catch (SQLException e) {
+      return false;
+    }
+
+    return true;
+  }
+
  }
