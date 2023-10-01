c61c65a93f93157f1697eda62d446858308bc2e2
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2023-10-01 20:45:34 -0300

    evento dao

---
+++ src/main/java/com/casaculturaqxd/sgec/DAO/EventoDAO.java
@@ -0,0 +1,11 @@
+package com.casaculturaqxd.sgec.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.SortedSet;
+
+public class EventoDAO {
+  
+
+ }
