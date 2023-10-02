4445c2a40efbecc641fbb6459ca5a4fb715bc820
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-10-02 00:13:56 -0300

    adicionando opcao de preencher dinamicamente as tabelas

--- src/main/java/com/casaculturaqxd/sgec/controller/VisualizarEventoController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/VisualizarEventoController.java
@@ -1,11 +1,15 @@
 package com.casaculturaqxd.sgec.controller;
 
 import com.casaculturaqxd.sgec.models.Evento;
+import com.casaculturaqxd.sgec.models.Indicador;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 
 public class VisualizarEventoController {
@@ -20,9 +24,39 @@ public class VisualizarEventoController {
 
     //Tabelas de indicadores
     @FXML
-    TableView<String> tabelaIndicadoresGerais,tabelaIndicadoresMeta1,tabelaIndicadoresMeta2;
+    TableView<Indicador> tabelaIndicadoresGerais,tabelaIndicadoresMeta1,tabelaIndicadoresMeta2;
 
     public void initialize(){
+        Indicador numeroPublico = new Indicador("Quantidade de público", 0, 100);
+        Indicador numeroMestres = new Indicador("Número de mestres da cultura", 0, 15);
+        Indicador numeroMunicipios = new Indicador("Número de municípios", 3, 3);
+
+        loadTable(tabelaIndicadoresGerais);
+        loadTable(tabelaIndicadoresMeta1);
+        loadTable(tabelaIndicadoresMeta2);
+
+        addIndicador(tabelaIndicadoresGerais, numeroPublico);
+        addIndicador(tabelaIndicadoresMeta1, numeroMestres);
+        addIndicador(tabelaIndicadoresMeta2, numeroMunicipios);
     }
 
+
+    private void loadTable(TableView<Indicador> tabela){
+        TableColumn<Indicador,String> nomeIndicador = new TableColumn<>("Nome do indicador");
+        nomeIndicador.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        
+        TableColumn<Indicador,Integer> valorEsperado = new TableColumn<>("Valor esperado");
+        valorEsperado.setCellValueFactory(new PropertyValueFactory<>("valorEsperado"));
+        
+        TableColumn<Indicador,Integer> valorAlcancado = new TableColumn<>("Valor esperado");
+        valorAlcancado.setCellValueFactory(new PropertyValueFactory<>("valorEsperado"));
+
+        tabela.getColumns().add(nomeIndicador);
+        tabela.getColumns().add(valorEsperado);
+        tabela.getColumns().add(valorAlcancado);
+        tabela.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_FLEX_LAST_COLUMN);
+    }
+    private void addIndicador(TableView<Indicador> tabela, Indicador indicador){
+        tabela.setItems(FXCollections.observableArrayList(indicador));
+    }
 }
