2e78f0453366d6626e80346cf3eecbe8e2987c33
Robson <robsonad07@gmail.com>
2023-10-02 21:12:10 -0300

    metodo setDataInicial criado em Builder

--- src/main/java/com/builder/Builder.java
+++ src/main/java/com/builder/Builder.java
@@ -1,7 +1,10 @@
 package com.builder;
 
+import java.util.Date;
+
 public interface Builder {
   void resetar();
   void setNome(String nome);
   void setDescricao(String descricao);
+  void setDataInicial(Date dataInicial);
 }
