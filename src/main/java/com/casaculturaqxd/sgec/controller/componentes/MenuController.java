e36a66f85c4a50353cb6a95dc0e5effa42bdcb3c
Robson <robsonad07@gmail.com>
2023-10-03 20:53:50 -0300

    mudanças realizadas no MenuController

--- src/main/java/com/casaculturaqxd/sgec/controller/componentes/MenuController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/componentes/MenuController.java
@@ -1,5 +1,36 @@
 package com.casaculturaqxd.sgec.controller.componentes;
 
+import java.io.FileNotFoundException;
+import java.net.URL;
+
+import com.casaculturaqxd.sgec.App;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
 public class MenuController {
 
+    @FXML
+    VBox vboxmenu;
+
+    private FXMLLoader sceneLoader;
+
+    public void initialize() throws FileNotFoundException{
+        
+        String menu = "menu";
+        URL pagePath = App.class.getResource("view/" + menu + ".fxml");
+        if (pagePath == null) {
+            throw new FileNotFoundException(menu + ".fxml nao encontrado");
+        }
+        sceneLoader = new FXMLLoader(pagePath);
+        try {
+            Pane header = sceneLoader.load();
+            vboxmenu.getChildren().add(header);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Lidar com exceções, se necessário
+        }
+    } 
 }
