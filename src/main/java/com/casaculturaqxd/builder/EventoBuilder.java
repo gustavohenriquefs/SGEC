6eabc6583c45a979338c503919f90bbb4cb2a9df
Robson <robsonad07@gmail.com>
2023-10-02 21:31:44 -0300

    EventoBuilder.java criado

---
+++ src/main/java/com/casaculturaqxd/builder/EventoBuilder.java
@@ -0,0 +1,60 @@
+package com.casaculturaqxd.builder;
+
+import java.util.Date;
+import java.util.Set;
+
+import com.casaculturaqxd.sgec.models.Instituicao;
+
+public class EventoBuilder implements Builder {
+
+  @Override
+  public void resetar() {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setNome(String nome) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setDescricao(String descricao) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setDataInicial(Date dataInicial) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setDataFinal(Date dataFinal) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setPublicoEsperado(int publicoEsperado) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setPublicoAlcancado(int publicoAlcancado) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setClassificacaoEtaria(String classificacaoEtaria) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setColaboradores(Set<Instituicao> colaboradores) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void setOrganizadores(Set<Instituicao> organizadores) {
+    // TODO Auto-generated method stub
+  }
+  
+}
