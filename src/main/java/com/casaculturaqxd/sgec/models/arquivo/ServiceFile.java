faa677fa6f3c5a202e5044bd1679c3cb40011dbc
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-10-13 09:07:05 -0300

    adicionando model de arquivo e classe gerenciadora de arquivos

---
+++ src/main/java/com/casaculturaqxd/sgec/models/arquivo/ServiceFile.java
@@ -0,0 +1,66 @@
+package com.casaculturaqxd.sgec.models.arquivo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Date;
+
+import javafx.scene.image.Image;
+
+public class ServiceFile {
+    private Integer serviceFileId;
+    private String fileKey;
+    private String service;
+    private String bucket;
+    private Date ultimaModificacao;
+    private File content;
+    
+    public ServiceFile(File content,String bucket) {
+        this.content = content;
+        this.bucket = bucket;
+    }
+
+    public Image getContentPreview() throws FileNotFoundException{
+        return new Image(new FileInputStream(content));
+    }
+    
+    public Integer getServiceFileId() {
+        return serviceFileId;
+    }
+    public void setServiceFileId(Integer serviceFileId) {
+        this.serviceFileId = serviceFileId;
+    }
+    public String getFileKey() {
+        return fileKey;
+    }
+    public void setFileKey(String fileKey) {
+        this.fileKey = fileKey;
+    }
+    public String getService() {
+        return service;
+    }
+    public void setService(String service) {
+        this.service = service;
+    }
+    public String getBucket() {
+        return bucket;
+    }
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+    public File getContent() {
+        return content;
+    }
+    public void setContent(File content) {
+        this.content = content;
+    }
+
+    public Date getUltimaModificacao() {
+        return ultimaModificacao;
+    }
+
+    public void setUltimaModificacao(Date ultimaModificacao) {
+        this.ultimaModificacao = ultimaModificacao;
+    }
+
+}
