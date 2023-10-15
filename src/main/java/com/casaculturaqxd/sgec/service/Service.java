71827351ad0cc1185feced3c83361eec2d6d79c3
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-10-15 16:28:09 -0300

    formatando documentacao

--- src/main/java/com/casaculturaqxd/sgec/service/Service.java
+++ src/main/java/com/casaculturaqxd/sgec/service/Service.java
@@ -8,48 +8,62 @@ import com.casaculturaqxd.sgec.models.arquivo.ServiceFile;
 
 public interface Service {
    /**
-    * Cria um novo bucket com o nome especificado, se já 
+    * Cria um novo bucket com o nome especificado, se já
     * existir um bucket com esse nome lança uma exceção
+    * 
     * @param nomeBucket
     * @throws IllegalArgumentException
     */
    public void criarBucket(String nomeBucket) throws IllegalArgumentException;
-   
+
    /**
-    * Deleta o bucket com o nome passado, se não existir 
-    * nenhum bucket com esse nome lança uma execeção
+    * Deleta o bucket com o nome passado, se não existir
+    * nenhum bucket com esse nome lança uma exceção
+    * 
     * @param nomeBucket
     * @throws IllegalArgumentException
     */
    public void deletarBucket(String nomeBucket) throws IllegalArgumentException;
 
-   public ServiceFile getArquivo(String nomeBucket, String chaveArquivo);
+   /**
+    * Retorna um arquivo individual convertido para uma
+    * ServiceFile, levanta uma exceção caso o nome do bucket
+    * ou do arquivo sejam inválidos
+    * 
+    * @param nomeBucket
+    * @param chaveArquivo
+    * @throws IllegalArgumentException
+    */
+   public ServiceFile getArquivo(String nomeBucket, String chaveArquivo) throws IllegalArgumentException;
 
    /**
     * realiza upload do arquivo para o bucket especificado,
     * salvando-o com a chave de destino escolhida
     * retorna uma exceção caso o bucket ou o arquivo
     * sejam inválidos
+    * 
     * @param nomeBucket
     * @param chaveDestino
     * @param arquivo
     * @throws IllegalArgumentException
     * @throws IOException
     */
-   public void enviarArquivo(String nomeBucket,String chaveDestino ,File arquivo) throws IllegalArgumentException,IOException;
+   public void enviarArquivo(String nomeBucket, String chaveDestino, File arquivo)
+         throws IllegalArgumentException, IOException;
 
    /**
-    * Retorna uma lista de todos os arquivos no bucket especificado
+    * Retorna uma lista com o nome de todos os arquivos no bucket especificado
+    * 
     * @param nomeBucket
     * @throws IllegalArgumentException
     */
    public List<String> listarArquivos(String nomeBucket) throws IllegalArgumentException;
 
-
    /**
     * Remove o arquivo com a chave especificada do bucket
-    * lança uma exceção caso a chave ou o nome do bucket 
+    * lança uma exceção caso a chave ou o nome do bucket
     * sejam inválidos
+    * 
     * @param nomeBucket
     * @param chaveArquivo
     * @throws IllegalArgumentException
