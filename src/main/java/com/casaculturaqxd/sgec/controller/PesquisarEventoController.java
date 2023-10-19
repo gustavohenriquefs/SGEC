3e3da769898a4ec0e5cedd309566f294893dfcfc
Robson <robsonad07@gmail.com>
2023-10-19 20:07:37 -0300

    modificações feitas em PesquisarEventoController

--- src/main/java/com/casaculturaqxd/sgec/controller/PesquisarEventoController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/PesquisarEventoController.java
@@ -6,16 +6,61 @@ import com.casaculturaqxd.sgec.App;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
 public class PesquisarEventoController {
     @FXML
+    AnchorPane root;
+    @FXML
     HBox header;
 
     public void initialize() throws IOException{
-        FXMLLoader loaderMenu = new FXMLLoader(App.class.getResource("view/componentes/menu.fxml"));
-        header.getChildren().add(loaderMenu.load());
+        loadMenu();
     }
+
+    private void loadMenu() throws IOException{
+        FXMLLoader carregarMenu = new FXMLLoader(App.class.getResource("view/componentes/menu.fxml"));
+        root.getChildren().add(1,carregarMenu.load());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 }
