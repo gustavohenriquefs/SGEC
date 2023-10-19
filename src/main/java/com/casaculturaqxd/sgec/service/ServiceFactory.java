3384df22ef6961e28abb5d23c696dc8283eefbbc
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-10-19 19:00:32 -0300

    implementando enumeracao em serviceFactory

--- src/main/java/com/casaculturaqxd/sgec/service/ServiceFactory.java
+++ src/main/java/com/casaculturaqxd/sgec/service/ServiceFactory.java
@@ -1,11 +1,15 @@
 package com.casaculturaqxd.sgec.service;
 
+import com.casaculturaqxd.sgec.enums.ServiceType;
+
 public class ServiceFactory {
-    public static Service getService(String type, String envAccessKey, String envSecretKey){
-        if(type.equalsIgnoreCase("S3") ){
+    
+    public static Service getService(ServiceType type, String envAccessKey, String envSecretKey){
+        if(type.toString().equalsIgnoreCase("S3") ){
             return new S3Service(envAccessKey, envSecretKey);
         }
 
         return null;
     }
+
 }
