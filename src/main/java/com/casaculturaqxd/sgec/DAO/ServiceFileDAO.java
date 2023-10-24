efddebee972ffb61fb89b68cc7d4f4087354c954
Robson <robsonad07@gmail.com>
2023-10-23 23:00:51 -0300

    Atributos e construtor criados

--- src/main/java/com/casaculturaqxd/sgec/DAO/ServiceFileDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/ServiceFileDAO.java
@@ -1,5 +1,20 @@
 package com.casaculturaqxd.sgec.DAO;
 
+import java.sql.Connection;
+
+import com.casaculturaqxd.sgec.enums.ServiceType;
+import com.casaculturaqxd.sgec.service.Service;
+
 public class ServiceFileDAO {
-  
+  String suffix;
+  ServiceType serviceType;
+  Connection connection;
+  Service service;
+  String bucket;
+
+  ServiceFileDAO(String bucket, Service service){
+    this.bucket = bucket;
+    this.service = service;
+    serviceType = ServiceType.S3;
+  }
 }
