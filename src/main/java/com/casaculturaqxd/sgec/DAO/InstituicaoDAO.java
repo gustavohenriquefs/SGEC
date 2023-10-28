f4e8d5d777baac3989f720e79598b0fcdbb294ef
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-10-28 09:51:06 -0300

    adicionando metodos de vinculo e desvinculo, corrigindo insert, get e delete

--- src/main/java/com/casaculturaqxd/sgec/DAO/InstituicaoDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/InstituicaoDAO.java
@@ -4,13 +4,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
 import java.util.Optional;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import com.casaculturaqxd.sgec.models.Instituicao;
 
 public class InstituicaoDAO {
   private Connection conn;
-  
+
+  public Connection getConn() {
+    return conn;
+  }
+
   public void setConnection(Connection conn) {
     this.conn = conn;
 
@@ -30,12 +37,10 @@ public class InstituicaoDAO {
       statement.setInt(1, instituicao.getIdInstituicao());
 
       ResultSet resultado = statement.executeQuery();
-      
+
       if (resultado.next()) {
-        instituicao.setIdInstituicao(resultado.getInt("id_instituicao"));
         instituicao.setNome(resultado.getString("nome_instituicao"));
-        instituicao.setDescricaoContribuicao(resultado.getString("descricao_contribuicao"));
-        instituicao.setValorContribuicao(resultado.getInt("valor_contribuicao"));
+        instituicao.setIdServiceFile(resultado.getInt("id_service_file"));
       }
 
       statement.close();
@@ -47,16 +52,22 @@ public class InstituicaoDAO {
   }
 
   public boolean inserirInstituicao(Instituicao instituicao) throws SQLException {
-    String inserirInstituicaoQuery = "INSERT INTO instituicao (nome_instituicao, descricao_contribuicao, valor_contribuicao) VALUES (?, ?, ?)";
-    
-    try {
-      PreparedStatement statement = conn.prepareStatement(inserirInstituicaoQuery);
-      
-      statement.setString(1, instituicao.getNome());
-      statement.setString(2, instituicao.getDescricaoContribuicao());
-      statement.setFloat(3, instituicao.getValorContribuicao());
+    String inserirInstituicaoQuery =
+        "INSERT INTO instituicao (nome_instituicao, id_service_file) VALUES (?, ?)";
 
-      statement.execute();
+    try {
+      PreparedStatement statement =
+          conn.prepareStatement(inserirInstituicaoQuery, Statement.RETURN_GENERATED_KEYS);
+
+      statement.setString(1, instituicao.getNome());
+      statement.setObject(2, instituicao.getIdServiceFile(), Types.INTEGER);
+
+      statement.executeUpdate();
+
+      ResultSet rs = statement.getGeneratedKeys();
+      if (rs.next()) {
+        instituicao.setIdInstituicao(rs.getInt("id_instituicao"));
+      }
       statement.close();
     } catch (Exception e) {
       return false;
@@ -66,14 +77,14 @@ public class InstituicaoDAO {
   }
 
   public boolean atualizarInstituicao(Instituicao instituicao) throws SQLException {
-    String atualizarInstituicaoQuery = "UPDATE instituicao SET nome_instituicao=?, descricao_contribuicao=?, valor_contribuicao=? WHERE id_instituicao=?";
+    String atualizarInstituicaoQuery =
+        "UPDATE instituicao SET nome_instituicao=?, id_service_file=? WHERE id_instituicao=?";
 
     try {
       PreparedStatement statement = conn.prepareStatement(atualizarInstituicaoQuery);
 
       statement.setString(1, instituicao.getNome());
-      statement.setString(2, instituicao.getDescricaoContribuicao());
-      statement.setFloat(3, instituicao.getValorContribuicao());
+      statement.setInt(2, instituicao.getIdServiceFile());
       statement.setInt(4, instituicao.getIdInstituicao());
 
       statement.execute();
@@ -93,12 +104,148 @@ public class InstituicaoDAO {
 
       statement.setInt(1, instituicao.getIdInstituicao());
 
-      statement.execute();
+      int numRemocoes = statement.executeUpdate();
       statement.close();
+
+      return numRemocoes > 0;
     } catch (Exception e) {
       return false;
     }
+  }
 
-    return true;
+  public boolean vincularOrganizador(Instituicao instituicao, Integer idEvento) {
+    try {
+      String sql =
+          "insert into organizador_evento (id_instituicao,id_evento,descricao_contribuicao, valor_contribuicao)"
+              + " values(?, ?, ?, ?)";
+      PreparedStatement stmt = conn.prepareStatement(sql);
+      stmt.setInt(1, instituicao.getIdInstituicao());
+      stmt.setInt(2, idEvento);
+      stmt.setString(3, instituicao.getDescricaoContribuicao());
+      stmt.setString(4, instituicao.getValorContribuicao());
+      stmt.execute();
+      stmt.close();
+      return true;
+    } catch (SQLException e) {
+      return false;
+    } catch (NullPointerException e) {
+      Logger erro = Logger.getLogger("ID faltando");
+      erro.log(Level.SEVERE, "excecao levantada:", e);
+      return false;
+    }
+  }
+
+  public boolean vincularOrganizador(Integer idInstituicao, Integer idEvento,
+      String descricaoContribuicao, String valorContribuicao) {
+    try {
+      String sql =
+          "insert into organizador_evento (id_instituicao,id_evento,descricao_contribuicao, valor_contribuicao)"
+              + " values(?, ?, ?, ?)";
+      PreparedStatement stmt = conn.prepareStatement(sql);
+      stmt.setInt(1, idInstituicao);
+      stmt.setInt(2, idEvento);
+      stmt.setString(3, descricaoContribuicao);
+      stmt.setString(4, valorContribuicao);
+      stmt.execute();
+      stmt.close();
+      return true;
+    } catch (SQLException e) {
+      return false;
+    } catch (NullPointerException e) {
+      Logger erro = Logger.getLogger("ID faltando");
+      erro.log(Level.SEVERE, "excecao levantada:", e);
+      return false;
+    }
+  }
+
+  public boolean vincularColaborador(Instituicao instituicao, Integer idEvento) {
+    try {
+      String sql =
+          "insert into colaborador_evento (id_instituicao,id_evento,descricao_contribuicao, valor_contribuicao)"
+              + " values(?, ?, ?, ?)";
+      PreparedStatement stmt = conn.prepareStatement(sql);
+      stmt.setInt(1, instituicao.getIdInstituicao());
+      stmt.setInt(2, idEvento);
+      stmt.setString(3, instituicao.getDescricaoContribuicao());
+      stmt.setString(4, instituicao.getValorContribuicao());
+      stmt.execute();
+      stmt.close();
+      return true;
+    } catch (SQLException e) {
+      return false;
+    } catch (NullPointerException e) {
+      Logger erro = Logger.getLogger("ID faltando");
+      erro.log(Level.SEVERE, "excecao levantada:", e);
+      return false;
+    }
+  }
+
+  public boolean vincularColaborador(Integer idInstituicao, Integer idEvento,
+      String descricaoContribuicao, String valorContribuicao) {
+    try {
+      String sql =
+          "insert into colaborador_evento (id_instituicao,id_evento,descricao_contribuicao, valor_contribuicao)"
+              + " values(?, ?, ?, ?)";
+      PreparedStatement stmt = conn.prepareStatement(sql);
+      stmt.setInt(1, idInstituicao);
+      stmt.setInt(2, idEvento);
+      stmt.setString(3, descricaoContribuicao);
+      stmt.setString(4, valorContribuicao);
+      stmt.execute();
+      stmt.close();
+      return true;
+    } catch (SQLException e) {
+      return false;
+    } catch (NullPointerException e) {
+      Logger erro = Logger.getLogger("ID faltando");
+      erro.log(Level.SEVERE, "excecao levantada:", e);
+      return false;
+    }
+  }
+
+  public boolean desvincularOrganizador(Integer idInstituicao, Integer idEvento) {
+    String vincLocaisSql = "DELETE FROM organizador_evento WHERE id_instituicao=? AND id_evento=?";
+
+    try {
+      PreparedStatement stmt = conn.prepareStatement(vincLocaisSql);
+      stmt.setInt(1, idInstituicao);
+      stmt.setInt(2, idEvento);
+
+      int numRemocoes = stmt.executeUpdate();
+      stmt.close();
+      return numRemocoes > 0;
+
+    } catch (SQLException e) {
+      Logger erro = Logger.getLogger("erroSQl");
+      erro.log(Level.SEVERE, "excecao levantada:", e);
+      return false;
+    } catch (NullPointerException e) {
+      Logger erro = Logger.getLogger("ID faltando");
+      erro.log(Level.SEVERE, "excecao levantada:", e);
+      return false;
+    }
+  }
+
+  public boolean desvincularColaborador(Integer idInstituicao, Integer idEvento) {
+    String vincLocaisSql = "DELETE FROM colaborador_evento WHERE id_instituicao=? AND id_evento=?";
+
+    try {
+      PreparedStatement stmt = conn.prepareStatement(vincLocaisSql);
+      stmt.setInt(1, idInstituicao);
+      stmt.setInt(2, idEvento);
+
+      int numRemocoes = stmt.executeUpdate();
+      stmt.close();
+      return numRemocoes > 0;
+
+    } catch (SQLException e) {
+      Logger erro = Logger.getLogger("erroSQl");
+      erro.log(Level.SEVERE, "excecao levantada:", e);
+      return false;
+    } catch (NullPointerException e) {
+      Logger erro = Logger.getLogger("ID faltando");
+      erro.log(Level.SEVERE, "excecao levantada:", e);
+      return false;
+    }
   }
 }
