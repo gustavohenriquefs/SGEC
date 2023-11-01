3ab75d316c3210299bd1f62c4c961afb33f7269c
Joao-Pedro-P-Holanda <110267046+Joao-Pedro-P-Holanda@users.noreply.github.com>
2023-11-01 11:00:40 -0300

    Test/database postgres (#94)
    * consertando typo
    
    * criando classe de teste, erro encontrado em variavelEnvExists
    
    * removendo teste com instancia invalida
    
    * removendo sysout
    
    * adicionando construtor
    
    * adicionando visualizacao das classes de teste detectadas
    
    * mudando privacidade do construtor
    
    * corrigindo maven.yml
    
    * inicializando variaveis no construtor tambem

---
+++ src/test/java/com/casaculturaqxd/sgec/jdbc/TestDatabasePostgres.java
@@ -0,0 +1,62 @@
+package com.casaculturaqxd.sgec.jdbc;
+
+import java.sql.Connection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestDatabasePostgres {
+    DatabasePostgres database;
+    String INVALID_ENV = "";
+
+    public TestDatabasePostgres() {
+        database = DatabasePostgres.getInstance("URL_TEST", "USER_NAME_TEST", "PASSWORD_TEST");
+        INVALID_ENV = "";
+    }
+
+    @BeforeEach
+    public void setUp() throws IncorrectEnvironmentVariableException {
+        database = DatabasePostgres.getInstance("URL_TEST", "USER_NAME_TEST", "PASSWORD_TEST");
+    }
+
+
+    @Test
+    public void testUniqueInstance() {
+        Connection firstConnection = database.getConnection();
+        Connection secondConnection = database.getConnection();
+        assertEquals(firstConnection, secondConnection);
+    }
+
+
+    @Test
+    public void testDesconectarOnValidConnection() {
+        database.getConnection();
+        assertDoesNotThrow(() -> {
+            database.desconectar(database.getConnection());
+        });
+    }
+
+    @Test
+    public void testSetInvalidUrlDataBase() throws IncorrectEnvironmentVariableException {
+        assertThrows(IncorrectEnvironmentVariableException.class, () -> {
+            database.setUrlDataBase(INVALID_ENV);
+        });
+    }
+
+    @Test
+    public void testSetInvalidNomeUsuario() {
+        assertThrows(IncorrectEnvironmentVariableException.class, () -> {
+            database.setNomeUsuario(INVALID_ENV);
+        });
+    }
+
+    @Test
+    public void testSetInvalidSenha() {
+        assertThrows(IncorrectEnvironmentVariableException.class, () -> {
+            database.setSenha(INVALID_ENV);
+        });
+    }
+
+}
