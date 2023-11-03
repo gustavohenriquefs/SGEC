b3789817afb393508e75a296a3fd697493c527be
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-11-03 11:12:48 -0300

    adicionando metodo getStage

--- src/main/java/com/casaculturaqxd/sgec/controller/ControllerEvento.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/ControllerEvento.java
@@ -1,6 +1,7 @@
 package com.casaculturaqxd.sgec.controller;
 
 import com.casaculturaqxd.sgec.models.Participante;
+import javafx.stage.Stage;
 
 /**
  * Interface para os controllers das telas relacionadas a eventos individuais
@@ -10,4 +11,6 @@ public interface ControllerEvento {
 
     public void removerParticipante(Participante participante);
 
+    // retorna o stage da tela, necessario para abrir o FileChooser
+    public Stage getStage();
 }
