db72eff141f5cca578850b1235446dfe597c2553
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-11-05 11:15:23 -0300

    adicionando interface para arquivos e listener do map de arquivos

--- src/main/java/com/casaculturaqxd/sgec/controller/CadastrarEventoController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/CadastrarEventoController.java
@@ -1,7 +1,10 @@
 package com.casaculturaqxd.sgec.controller;
 
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -31,48 +34,54 @@ import java.util.TreeSet;
 import com.casaculturaqxd.sgec.App;
 import com.casaculturaqxd.sgec.DAO.EventoDAO;
 import com.casaculturaqxd.sgec.builder.EventoBuilder;
+import com.casaculturaqxd.sgec.controller.preview.PreviewArquivoController;
 import com.casaculturaqxd.sgec.jdbc.DatabasePostgres;
 import com.casaculturaqxd.sgec.models.Evento;
+import com.casaculturaqxd.sgec.models.Participante;
+import com.casaculturaqxd.sgec.models.arquivo.ServiceFile;
 
 
-public class CadastrarEventoController {
+public class CadastrarEventoController implements ControllerServiceFile {
     private final int MAX_LOCALIZACOES = 4;
-    DatabasePostgres db = DatabasePostgres.getInstance("URL","USER_NAME","PASSWORD");
+    DatabasePostgres db = DatabasePostgres.getInstance("URL", "USER_NAME", "PASSWORD");
     EventoBuilder builderEvento = new EventoBuilder();
     EventoDAO eventoDAO = new EventoDAO();
-    ArrayList<FieldLocalizacaoController> controllersLocais = new ArrayList<FieldLocalizacaoController>();
+    ArrayList<FieldLocalizacaoController> controllersLocais =
+            new ArrayList<FieldLocalizacaoController>();
     DateFormat formatterHorario;
     Stage stage;
     @FXML
     VBox root;
     @FXML
     VBox Localizacoes, cargaHoraria;
-    @FXML 
-    HBox header, Participantes, Organizadores, Colaboradores;
     @FXML
-    TextField titulo, publicoEsperado, publicoAlcancado, horas, minutos, horasCargaHoraria ,numParticipantesEsperado,numMunicipiosEsperado;
+    HBox header, Participantes, Organizadores, Colaboradores, secaoArquivos;
+    @FXML
+    TextField titulo, publicoEsperado, publicoAlcancado, horas, minutos, horasCargaHoraria,
+            numParticipantesEsperado, numMunicipiosEsperado;
     @FXML
     TextArea descricao;
-    @FXML 
+    @FXML
     DatePicker dataInicial, dataFinal;
     @FXML
     ChoiceBox<String> classificacaoEtaria;
-    private String[] classificacoes = {"Livre","10 anos","12 anos","14 anos","16 anos","18 anos"};
+    private String[] classificacoes =
+            {"Livre", "10 anos", "12 anos", "14 anos", "16 anos", "18 anos"};
     @FXML
     CheckBox checkMeta1, checkMeta2, checkMeta3, checkMeta4;
     @FXML
     RadioButton certificavel, acessivelEmLibras;
-
-    //Botoes
+    private ObservableMap<ServiceFile, FXMLLoader> mapServiceFiles;
+    // Botoes
     @FXML
-    Button botaoNovaLocalizacao, botaoOrganizadores, botaoColaboradores,
-    botaoParticipantes, botaoArquivos, cancelar, criarEvento;
+    Button botaoNovaLocalizacao, botaoOrganizadores, botaoColaboradores, botaoParticipantes,
+            botaoArquivos, cancelar, criarEvento;
 
-    public void initialize() throws IOException{
+    public void initialize() throws IOException {
         formatterHorario = new SimpleDateFormat("HH:mm");
 
         loadMenu();
-        //inicia com o local obrigatorio carregado na pagina
+        // inicia com o local obrigatorio carregado na pagina
         carregarCampoLocalizacao();
         classificacaoEtaria.getItems().addAll(classificacoes);
         addInputConstraints();
@@ -81,75 +90,80 @@ public class CadastrarEventoController {
         showCertificavel(checkMeta3.isSelected());
     }
 
-    private void loadMenu() throws IOException{
-        FXMLLoader carregarMenu = new FXMLLoader(App.class.getResource("view/componentes/menu.fxml"));
-        root.getChildren().add(0,carregarMenu.load());
+    private void loadMenu() throws IOException {
+        FXMLLoader carregarMenu =
+                new FXMLLoader(App.class.getResource("view/componentes/menu.fxml"));
+        root.getChildren().add(0, carregarMenu.load());
     }
 
-    public void criarNovoEvento() throws IOException, ParseException{
-        if(emptyLocalizacoes()){
-            Alert erroLocalizacao = new Alert(AlertType.ERROR, "Um evento deve possuir pelo menos uma localização associada");
+    public void criarNovoEvento() throws IOException, ParseException {
+        if (emptyLocalizacoes()) {
+            Alert erroLocalizacao = new Alert(AlertType.ERROR,
+                    "Um evento deve possuir pelo menos uma localização associada");
             erroLocalizacao.show();
         }
-        if(!camposObrigatoriosPreenchidos()){
-            Alert erroLocalizacao = new Alert(AlertType.ERROR, "nem todos os campos obrigatorios foram preenchidos");
+        if (!camposObrigatoriosPreenchidos()) {
+            Alert erroLocalizacao = new Alert(AlertType.ERROR,
+                    "nem todos os campos obrigatorios foram preenchidos");
             erroLocalizacao.show();
         }
 
         builderEvento.resetar();
         eventoDAO.setConnection(db.getConnection());
 
-            builderEvento.setNome(titulo.getText());
-            builderEvento.setDescricao(titulo.getText());
-            builderEvento.setClassificacaoEtaria(classificacaoEtaria.getSelectionModel().getSelectedItem());
-            if(dataInicial.getValue() != null){
-            builderEvento.setDataInicial( Date.valueOf(dataInicial.getValue()));
-            }
-            if(dataFinal.getValue() != null){
+        builderEvento.setNome(titulo.getText());
+        builderEvento.setDescricao(titulo.getText());
+        builderEvento
+                .setClassificacaoEtaria(classificacaoEtaria.getSelectionModel().getSelectedItem());
+        if (dataInicial.getValue() != null) {
+            builderEvento.setDataInicial(Date.valueOf(dataInicial.getValue()));
+        }
+        if (dataFinal.getValue() != null) {
             builderEvento.setDataFinal(Date.valueOf(dataFinal.getValue()));
-            }
-            if(!publicoEsperado.getText().isEmpty()){
+        }
+        if (!publicoEsperado.getText().isEmpty()) {
             builderEvento.setPublicoEsperado(Integer.parseInt(publicoEsperado.getText()));
-            }
-            if(!publicoAlcancado.getText().isEmpty()){
-            builderEvento.setPublicoAlcancado(Integer.parseInt(publicoAlcancado.getText()));    
-            }
-            builderEvento.setCertificavel(certificavel.isSelected());
-            if(!horasCargaHoraria.getText().isEmpty()){
-            builderEvento.setCargaHoraria(new java.sql.Time(formatterHorario.parse(horasCargaHoraria.getText()+":00:00").getTime()));
-            }
-            builderEvento.setAcessivelEmLibras(acessivelEmLibras.isSelected());
-            if(!numMunicipiosEsperado.getText().isEmpty()){
-                builderEvento.setMunicipiosEsperado(Integer.parseInt(numMunicipiosEsperado.getText()));
-            }
-            if(!numParticipantesEsperado.getText().isEmpty()){
-            builderEvento.setParticipantesEsperado(Integer.parseInt(numParticipantesEsperado.getText()));
-            }
+        }
+        if (!publicoAlcancado.getText().isEmpty()) {
+            builderEvento.setPublicoAlcancado(Integer.parseInt(publicoAlcancado.getText()));
+        }
+        builderEvento.setCertificavel(certificavel.isSelected());
+        if (!horasCargaHoraria.getText().isEmpty()) {
+            builderEvento.setCargaHoraria(new java.sql.Time(
+                    formatterHorario.parse(horasCargaHoraria.getText() + ":00:00").getTime()));
+        }
+        builderEvento.setAcessivelEmLibras(acessivelEmLibras.isSelected());
+        if (!numMunicipiosEsperado.getText().isEmpty()) {
+            builderEvento.setMunicipiosEsperado(Integer.parseInt(numMunicipiosEsperado.getText()));
+        }
+        if (!numParticipantesEsperado.getText().isEmpty()) {
+            builderEvento
+                    .setParticipantesEsperado(Integer.parseInt(numParticipantesEsperado.getText()));
+        }
         TreeSet<Integer> idLocais = new TreeSet<>();
-        for(FieldLocalizacaoController controller : controllersLocais){
+        for (FieldLocalizacaoController controller : controllersLocais) {
             idLocais.add(controller.getLocalizacao().getIdLocalizacao());
         }
         builderEvento.setLocalizacoes(idLocais);
         Evento novoEvento = builderEvento.getEvento();
-        if(eventoDAO.inserirEvento(novoEvento)){
+        if (eventoDAO.inserirEvento(novoEvento)) {
             novoEvento = eventoDAO.buscarEvento(novoEvento).get();
             App.setRoot("view/home");
         }
 
     }
-             
 
-    public void cancelar() throws IOException{
-        /** 
-         * TODO: implementar uma fila na classe App, para retornar a ultima
-         * tela visitada
+
+    public void cancelar() throws IOException {
+        /**
+         * TODO: implementar uma fila na classe App, para retornar a ultima tela visitada
          */
         builderEvento.resetar();
         App.setRoot("view/home");
     }
 
-    public void carregarCampoLocalizacao() throws IOException{
-        if(Localizacoes.getChildren().size() >= MAX_LOCALIZACOES){
+    public void carregarCampoLocalizacao() throws IOException {
+        if (Localizacoes.getChildren().size() >= MAX_LOCALIZACOES) {
             botaoNovaLocalizacao.setDisable(true);
         }
         SubSceneLoader loaderLocais = new SubSceneLoader();
@@ -159,44 +173,61 @@ public class CadastrarEventoController {
         controllersLocais.add(controller);
         System.out.println(controllersLocais);
     }
-    public void removerLocalizacao() throws IOException{
+
+    public void removerLocalizacao() throws IOException {
     }
-    
-    public void adicionarParticipante() throws IOException{
+
+    public void adicionarParticipante() throws IOException {
         SubSceneLoader loaderParticipantes = new SubSceneLoader();
         VBox novoParticipante = (VBox) loaderParticipantes.getPage("fields/fieldParticipante");
         Participantes.getChildren().add(novoParticipante);
     }
-    public void adicionarOrganizador() throws IOException{
-        //TODO: permitir adicionar uma instituicao existente, neste caso o preview dela
+
+    public void adicionarOrganizador() throws IOException {
+        // TODO: permitir adicionar uma instituicao existente, neste caso o preview dela
         // que e adicionado a pagina
         SubSceneLoader loaderOrganizadores = new SubSceneLoader();
-        AnchorPane novoOrganizador = (AnchorPane) loaderOrganizadores.getPage("fields/fieldInstituicao");
+        AnchorPane novoOrganizador =
+                (AnchorPane) loaderOrganizadores.getPage("fields/fieldInstituicao");
         Organizadores.getChildren().add(novoOrganizador);
 
     }
-    public void adicionarColaborador() throws IOException{
-        //TODO: permitir adicionar uma instituicao existente, neste caso o preview dela
+
+    public void adicionarColaborador() throws IOException {
+        // TODO: permitir adicionar uma instituicao existente, neste caso o preview dela
         // que e adicionado a pagina
         SubSceneLoader loaderColaboradores = new SubSceneLoader();
-        AnchorPane novoColaborador = (AnchorPane) loaderColaboradores.getPage("fields/fieldInstituicao");
+        AnchorPane novoColaborador =
+                (AnchorPane) loaderColaboradores.getPage("fields/fieldInstituicao");
         Colaboradores.getChildren().add(novoColaborador);
     }
-    public void adicionarArquivo(){
+
+    public void adicionarArquivo() {
         FileChooser fileChooser = new FileChooser();
         File arquivoSelecionado = fileChooser.showOpenDialog(stage);
+        adicionarArquivo(new ServiceFile(arquivoSelecionado, "sgecteste"));
     }
 
-    public boolean camposObrigatoriosPreenchidos(){
-        if(classificacaoEtaria.getSelectionModel().getSelectedItem() == null
-           || titulo.getText().isEmpty()
-           ||  dataInicial.getValue() == null 
-           || dataFinal.getValue() == null){
-            return false; 
+    @Override
+    public void adicionarArquivo(ServiceFile serviceFile) {
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarArquivo'");
+    }
+
+    @Override
+    public void removerArquivo(ServiceFile serviceFile) {
+        throw new UnsupportedOperationException("Unimplemented method 'removerArquivo'");
+    }
+
+    public boolean camposObrigatoriosPreenchidos() {
+        if (classificacaoEtaria.getSelectionModel().getSelectedItem() == null
+                || titulo.getText().isEmpty() || dataInicial.getValue() == null
+                || dataFinal.getValue() == null) {
+            return false;
         }
         return true;
     }
-    public void addInputConstraints(){
+
+    public void addInputConstraints() {
         /* aplicando restrições aos inputs */
         horas.setTextFormatter(getTimeFormatter());
         minutos.setTextFormatter(getTimeFormatter());
@@ -208,44 +239,79 @@ public class CadastrarEventoController {
 
     }
 
-    public TextFormatter<String> getNumericalFormatter(){
-        return new TextFormatter<>(change -> {
-            if(change.getText().matches("\\d+")){
-                return change;
+    public void addListenersServiceFile(ObservableMap<ServiceFile, FXMLLoader> observablemap) {
+        ControllerServiceFile superController = this;
+        observablemap.addListener(new MapChangeListener<ServiceFile, FXMLLoader>() {
+            @Override
+            public void onChanged(
+                    MapChangeListener.Change<? extends ServiceFile, ? extends FXMLLoader> change) {
+
+                if (change.wasAdded()) {
+                    ServiceFile addedKey = change.getKey();
+                    // carregar o fxml de preview e setar o ServiceFile deste para o
+                    // arquivo adicionado
+                    try {
+                        Parent previewParticipante = change.getValueAdded().load();
+                        PreviewArquivoController controller =
+                                change.getValueAdded().getController();
+                        controller.setServiceFile(addedKey);
+                        controller.setParentController(superController);
+
+                        secaoArquivos.getChildren().add(previewParticipante);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (change.wasRemoved()) {
+                    PreviewArquivoController removedController =
+                            change.getValueRemoved().getController();
+                    // Remover o Pane de preview ao deletar um Arquivo da lista
+                    secaoArquivos.getChildren().remove(removedController.getRoot());
+                }
             }
-            else{
-                change.setText(""); 
+        });
+    }
+
+    public TextFormatter<String> getNumericalFormatter() {
+        return new TextFormatter<>(change -> {
+            if (change.getText().matches("\\d+")) {
+                return change;
+            } else {
+                change.setText("");
 
                 return change;
             }
         });
     }
-    public TextFormatter<String> getTimeFormatter(){
+
+    public TextFormatter<String> getTimeFormatter() {
         return new TextFormatter<>(change -> {
-            if(change.getText().matches("\\d+") && change.getRangeEnd() < 2){
+            if (change.getText().matches("\\d+") && change.getRangeEnd() < 2) {
                 return change;
-            }   
-            else{
-                change.setText(""); 
+            } else {
+                change.setText("");
                 return change;
             }
         });
     }
 
-    public void showCargaHoraria(boolean value){
+    public void showCargaHoraria(boolean value) {
         cargaHoraria.setVisible(value);
     }
-    public void showCertificavel(boolean value){
-        if(value == false){
+
+    public void showCertificavel(boolean value) {
+        if (value == false) {
             certificavel.setSelected(value);
         }
         certificavel.setVisible(value);
     }
-    public void onClickMeta3(){
+
+    public void onClickMeta3() {
         showCargaHoraria(checkMeta3.isSelected());
         showCertificavel(checkMeta3.isSelected());
     }
-    public boolean emptyLocalizacoes(){
+
+    public boolean emptyLocalizacoes() {
         return Localizacoes.getChildren().isEmpty();
     }
 }
