ef608d798595f7f029efaa9af27882de3e91e046
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-11-08 11:58:13 -0300

    adicionando preview de arquivos

--- src/main/java/com/casaculturaqxd/sgec/controller/preview/PreviewArquivoController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/preview/PreviewArquivoController.java
@@ -1,32 +1,116 @@
 package com.casaculturaqxd.sgec.controller.preview;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Optional;
+
+import com.casaculturaqxd.sgec.controller.CadastrarEventoController;
 import com.casaculturaqxd.sgec.controller.ControllerServiceFile;
 import com.casaculturaqxd.sgec.models.arquivo.ServiceFile;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class PreviewArquivoController {
     private ServiceFile serviceFile;
-    private ControllerServiceFile ParentController;
+    private ControllerServiceFile parentController;
+    private Alert alertaArquivo;
+    private ButtonType showFileOption, closeOption;
+    private Stage stage;
     @FXML
     private Parent root;
     @FXML
     private ImageView imagemPreview;
     @FXML
     private Label fileKey, date, fileType, tamanho;
+    @FXML
+    private MenuItem downloadItem;
 
     public void initialize() {
+        loadAlertDialog();
+    }
+
+    public void download() throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Salvar arquivo");
+        fileChooser.getExtensionFilters()
+                .add(new ExtensionFilter("Tipo de arquivo", "*" + serviceFile.getSuffix()));
+        File downloadFile = fileChooser.showSaveDialog(stage);
+        if (!writeFileContent(downloadFile)) {
+            // mudando tipo de alerta e removendo opcao de visualizacao em caso de falha
+            alertaArquivo.setAlertType(AlertType.WARNING);
+            alertaArquivo.setContentText("Erro ao baixar arquivo");
+            alertaArquivo.getButtonTypes().remove(showFileOption);
+        }
+
+        executeCloseDialogAction(alertaArquivo.showAndWait(), downloadFile);
 
     }
 
-    public void download() {
+    private void executeCloseDialogAction(Optional<ButtonType> chosenButton, File result)
+            throws IOException {
+        if (chosenButton.isPresent()) {
+            if (chosenButton.get() == showFileOption) {
+                Desktop desktop = Desktop.getDesktop();
+                desktop.open(result.getParentFile());
+            }
+        }
 
+    }
+
+    private boolean writeFileContent(File file) {
+        try {
+            InputStream readFromContent = new FileInputStream(serviceFile.getContent());
+            OutputStream writeToFile = new FileOutputStream(file);
+            readFromContent.transferTo(writeToFile);
+            return true;
+        } catch (FileNotFoundException exception) {
+            return false;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private void loadAlertDialog() {
+        // inicializando dialog de confirmacao com opcao de ver o arquivo usando o file
+        // system do
+        // sistema ou fechar o aviso
+        showFileOption = new ButtonType("Visualizar", ButtonData.OK_DONE);
+        closeOption = new ButtonType("Fechar", ButtonData.CANCEL_CLOSE);
+        alertaArquivo = new Alert(AlertType.CONFIRMATION);
+        alertaArquivo.setContentText("Arquivo baixado com sucesso");
+        alertaArquivo.getButtonTypes().clear();
+        alertaArquivo.getButtonTypes().addAll(showFileOption, closeOption);
+    }
+
+    private void loadContent() {
+        if (serviceFile != null) {
+            String dataArquivo = serviceFile.getUltimaModificacao() == null ? " "
+                    : String.valueOf(serviceFile.getUltimaModificacao());
+            fileKey.setText(serviceFile.getFileKey());
+            tamanho.setText(formatFileSize(serviceFile.getContent().length()));
+            date.setText(dataArquivo);
+            fileType.setText(serviceFile.getSuffix());
+        }
     }
 
     public void deletar() {
-
+        parentController.removerArquivo(serviceFile);
     }
 
     public ServiceFile getServiceFile() {
@@ -36,14 +120,19 @@ public class PreviewArquivoController {
     public void setServiceFile(ServiceFile serviceFile) {
         imagemPreview.setImage(null);
         this.serviceFile = serviceFile;
+        loadContent();
     }
 
     public ControllerServiceFile getParentController() {
-        return ParentController;
+        return parentController;
     }
 
     public void setParentController(ControllerServiceFile parentController) {
-        ParentController = parentController;
+        if (parentController instanceof CadastrarEventoController) {
+            downloadItem.setVisible(false);
+            downloadItem.setDisable(true);
+        }
+        this.parentController = parentController;
     }
 
     public Parent getRoot() {
@@ -54,4 +143,12 @@ public class PreviewArquivoController {
         this.root = root;
     }
 
+    // recebe um tamanho de arquivo em bytes e retorna em KB, para tamanhos menores
+    // que 1024 KB, se
+    // nao retorna em MB
+    private String formatFileSize(long fileSize) {
+        long sizeInKB = fileSize / 1024;
+        long sizeInMB = sizeInKB / 1024;
+        return sizeInMB < 1 ? sizeInKB + " KB" : sizeInMB + " MB";
+    }
 }
