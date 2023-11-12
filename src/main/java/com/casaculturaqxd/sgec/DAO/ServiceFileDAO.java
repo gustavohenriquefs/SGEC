a49e8121bc19b59606f43dc4cd5a01d29a9f1cbc
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-11-12 16:13:18 -0300

    adicionando metodo de pesquisar arquivos de evento usando um nome

--- src/main/java/com/casaculturaqxd/sgec/DAO/ServiceFileDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/ServiceFileDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.SortedSet;
 
 import com.casaculturaqxd.sgec.models.Evento;
@@ -15,20 +16,20 @@ public class ServiceFileDAO {
   Connection connection;
   Service service;
 
-  ServiceFileDAO(Service service){
+  ServiceFileDAO(Service service) {
     this.service = service;
   }
 
-  public void setConnection(Connection connection){
+  public void setConnection(Connection connection) {
     this.connection = connection;
   }
 
-  public boolean inserirArquivo(ServiceFile arquivo){
+  public boolean inserirArquivo(ServiceFile arquivo) {
     try {
       setService(arquivo);
       service.enviarArquivo(arquivo);
       String sql = "insert into service_file (file_key,suffix,service,bucket,ultima_modificacao)"
-              + " values(?,?,?,?,?)";
+          + " values(?,?,?,?,?)";
       PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
       stmt.setString(1, arquivo.getFileKey());
       stmt.setString(2, arquivo.getSuffix());
@@ -38,7 +39,7 @@ public class ServiceFileDAO {
       int numRemocoes = stmt.executeUpdate();
       ResultSet rs = stmt.getGeneratedKeys();
       if (rs.next()) {
-          arquivo.setServiceFileId(rs.getInt("id_service_file"));
+        arquivo.setServiceFileId(rs.getInt("id_service_file"));
       }
       stmt.close();
       return numRemocoes > 0;
@@ -47,7 +48,7 @@ public class ServiceFileDAO {
     }
   }
 
-  public ServiceFile getArquivo(ServiceFile arquivo){
+  public ServiceFile getArquivo(ServiceFile arquivo) {
     try {
       setService(arquivo);
       service.getArquivo(arquivo.getBucket(), arquivo.getFileKey());
@@ -71,7 +72,7 @@ public class ServiceFileDAO {
     }
   }
 
-  public boolean deleteArquivo(ServiceFile arquivo){
+  public boolean deleteArquivo(ServiceFile arquivo) {
     try {
       setService(arquivo);
       service.deletarArquivo(arquivo.getBucket(), arquivo.getFileKey());
@@ -86,27 +87,27 @@ public class ServiceFileDAO {
     }
   }
 
-  public boolean vincularAllArquivos(Evento evento){
+  public boolean vincularAllArquivos(Evento evento) {
     SortedSet<Integer> listaArquivos = evento.getListaArquivos();
     for (Integer idServiceFile : listaArquivos) {
       boolean temp = vincularArquivo(idServiceFile, evento.getIdEvento());
-      if(temp == false)
+      if (temp == false)
         return false;
     }
     return true;
   }
 
-  public boolean vincularArquivo(int idServiceFile, int idEvento){
+  public boolean vincularArquivo(int idServiceFile, int idEvento) {
     String sql = "INSERT INTO service_file_evento(id_evento, id_service_file) VALUES (?, ?);";
     try {
-        PreparedStatement stmt = connection.prepareStatement(sql);
-        stmt.setInt(1, idEvento);
-        stmt.setInt(2, idServiceFile);
-        stmt.execute();
-        stmt.close();
-        return true;
+      PreparedStatement stmt = connection.prepareStatement(sql);
+      stmt.setInt(1, idEvento);
+      stmt.setInt(2, idServiceFile);
+      stmt.execute();
+      stmt.close();
+      return true;
     } catch (SQLException e) {
-        return false;
+      return false;
     }
   }
 
@@ -128,14 +129,35 @@ public class ServiceFileDAO {
     SortedSet<Integer> listaArquivos = evento.getListaArquivos();
     for (Integer idServiceFile : listaArquivos) {
       boolean temp = desvincularArquivo(idServiceFile, evento.getIdEvento());
-      if(temp == false)
+      if (temp == false)
         return false;
     }
     evento.setListaArquivos(null);
     return true;
   }
 
+  public ArrayList<ServiceFile> pesquisarArquivoEvento(String fileKey, int idEVento) {
+    String sql = "SELECT * FROM service_file_evento WHERE filekey ILIKE ? AND id_evento = ?";
+    ArrayList<ServiceFile> listaArquivos = new ArrayList<>();
+    try {
+      PreparedStatement statement = connection.prepareStatement(sql);
+      statement.setString(1, fileKey);
+      statement.setInt(2, idEVento);
+      ResultSet resultSet = statement.executeQuery();
+
+      while (resultSet.next()) {
+        ServiceFile serviceFile = new ServiceFile(resultSet.getInt("id_service_file"));
+        listaArquivos.add(getArquivo(serviceFile));
+      }
+      statement.close();
+      return listaArquivos;
+    } catch (Exception e) {
+      logException(e);
+      return null;
+    }
+  }
+
   public void setService(ServiceFile arquivo) {
-        this.service = arquivo.getService();
+    this.service = arquivo.getService();
   }
 }
