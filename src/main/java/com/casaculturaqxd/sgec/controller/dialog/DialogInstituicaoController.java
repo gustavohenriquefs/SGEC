03840470e3c99284916528a57f968422dd9bc6c2
Robson <robsonad07@gmail.com>
2023-11-14 20:45:40 -0300

    Atributos adicionados

--- src/main/java/com/casaculturaqxd/sgec/controller/dialog/DialogInstituicaoController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/dialog/DialogInstituicaoController.java
@@ -1,5 +1,27 @@
 package com.casaculturaqxd.sgec.controller.dialog;
 
+import com.casaculturaqxd.sgec.models.Instituicao;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class DialogInstituicaoController {
-  
+  @FXML
+  private TextField nomeInstituicao;
+
+  @FXML
+  private TextField contribuicoes;
+
+  @FXML
+  private TextField valorContribuicao;
+
+  @FXML
+  private ImageView imagem;
+
+  Dialog<Instituicao> dialog;
+  ObservableList<String> nomesInstituicoes;
+
 }
