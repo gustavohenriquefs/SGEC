820cc8104fec1be088110a740ee23dd9aacc432d
Joao-Pedro-P-Holanda <joaopedroph09@gmail.com>
2023-11-25 21:11:19 -0300

    corrigindo tratamento de excecoes e implementando optional

--- src/main/java/com/casaculturaqxd/sgec/DAO/UserDAO.java
+++ src/main/java/com/casaculturaqxd/sgec/DAO/UserDAO.java
@@ -9,12 +9,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Optional;
+
 import com.casaculturaqxd.sgec.models.User;
 
-public class UserDAO {
+public class UserDAO extends DAO {
   private Connection connection;
+
+  public UserDAO(Connection connection) {
+    this.connection = connection;
+  }
 
   public Connection getConnection() {
     return connection;
@@ -24,58 +28,68 @@ public class UserDAO {
     this.connection = connection;
   }
 
-  public boolean inserir(User obj) throws NoSuchAlgorithmException {
+  public boolean inserir(User user) throws SQLException {
+    String sql = "insert into usuario (nome_usuario,email,senha,editor)" + " values(?,?,?,?)";
+    PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
     try {
-      // 1° passo - criar comando sql
-      String sql = "insert into usuario (nome_usuario,email,senha,editor)" + " values(?,?,?,?)";
-      // 2° passo - conectar o banco de dados e organizar o comando sql
-      PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-      stmt.setString(1, obj.getNomeUsuario());
-      stmt.setString(2, obj.getEmail());
-      stmt.setString(3, encriptar(obj.getSenha()));
-      stmt.setBoolean(4, obj.isEditor());
-      // 3° passo - executar o comando sql
-      stmt.executeUpdate();
+      statement.setString(1, user.getNomeUsuario());
+      statement.setString(2, user.getEmail());
+      statement.setString(3, encriptar(user.getSenha()));
+      statement.setBoolean(4, user.isEditor());
 
-      ResultSet rs = stmt.getGeneratedKeys();
-      // caso a insercao seja realizada, atualiza o parametro com o id
+      int numInsercoes = statement.executeUpdate();
+
+      ResultSet rs = statement.getGeneratedKeys();
       if (rs.next()) {
-        obj.setIdUsuario(rs.getInt("id_usuario"));
+        user.setIdUsuario(rs.getInt("id_usuario"));
       }
-
-      stmt.close();
-      return true;
-    } catch (SQLException e) {
-      Logger erro = Logger.getLogger("erroSQl");
-      erro.log(Level.SEVERE, "excecao levantada:", e);
-      return false;
+      return numInsercoes > 0;
+    } catch (Exception e) {
+      String nomeUsuarioCausa = user.getNomeUsuario() != null ? user.getNomeUsuario() : "";
+      logException(e);
+      throw new SQLException("falha inserindo usuario " + nomeUsuarioCausa, e);
+    } finally {
+      statement.close();
     }
   }
 
-  public User getUsuario(User obj) {
-    String sql = "SELECT * FROM usuario WHERE id_usuario=?";
+  public Optional<User> getUsuario(User user) throws SQLException {
+    String sql = "SELECT id_usuario,nome_usuario,email,senha,editor FROM usuario WHERE id_usuario=?";
+    PreparedStatement statement = connection.prepareStatement(sql);
     try {
-      PreparedStatement stmt = connection.prepareStatement(sql);
-      stmt.setInt(1, obj.getIdUsuario());
-      ResultSet resultado = stmt.executeQuery();
+      statement.setInt(1, user.getIdUsuario());
+      ResultSet resultado = statement.executeQuery();
       if (resultado.next()) {
-        obj.setIdUsuario(resultado.getInt("id_usuario"));
-        obj.setNomeUsuario(resultado.getString("nome_usuario"));
-        obj.setEmail(resultado.getString("email"));
-        obj.setSenha(resultado.getString("senha"));
-        obj.setEditor(resultado.getBoolean("editor"));
-        return obj;
+        user.setIdUsuario(resultado.getInt("id_usuario"));
+        user.setNomeUsuario(resultado.getString("nome_usuario"));
+        user.setEmail(resultado.getString("email"));
+        user.setSenha(resultado.getString("senha"));
+        user.setEditor(resultado.getBoolean("editor"));
+        return Optional.of(user);
       } else {
-        return null;
+        return Optional.empty();
       }
-    } catch (SQLException e) {
-      Logger erro = Logger.getLogger("erroSQl");
-      erro.log(Level.SEVERE, "excecao levantada:", e);
+    } catch (Exception e) {
+      logException(e);
+      throw new SQLException("", e);
+    } finally {
+      statement.close();
+    }
+
+  }
+
+  /**
+   * Encripta a string passada como senha
+   * 
+   * @param senha
+   * @return
+   * @throws NoSuchAlgorithmException
+   */
+  private String encriptar(String senha) throws NoSuchAlgorithmException {
+    if (senha == null) {
       return null;
     }
-  }
-
-  private String encriptar(String senha) throws NoSuchAlgorithmException {
     MessageDigest digest = MessageDigest.getInstance("SHA-256");
     byte[] asHex = digest.digest(senha.getBytes(StandardCharsets.UTF_8));
     BigInteger number = new BigInteger(1, asHex);
@@ -83,62 +97,65 @@ public class UserDAO {
     return hexBuilder.toString();
   }
 
-  public boolean validar(User obj) throws NoSuchAlgorithmException {
-    String sql = "SELECT * FROM usuario WHERE email=? and senha=?";
-    try {
-      PreparedStatement stmt = connection.prepareStatement(sql);
-      stmt.setString(1, obj.getEmail());
-      stmt.setString(2, encriptar(obj.getSenha()));
-      ResultSet resultado = stmt.executeQuery();
-      if (resultado.next()) {
-        obj.setIdUsuario(resultado.getInt("id_usuario"));
-        obj.setNomeUsuario(resultado.getString("nome_usuario"));
-        obj.setEmail(resultado.getString("email"));
-        obj.setSenha(resultado.getString("senha"));
-        obj.setEditor(resultado.getBoolean("editor"));
+  public boolean validar(User user) throws SQLException {
+    String sql = "SELECT id_usuario,nome_usuario,email,senha,editor FROM usuario WHERE email=? and senha=?";
+    PreparedStatement statement = connection.prepareStatement(sql);
 
-        this.connection.setReadOnly(!obj.isEditor());
+    try {
+      statement.setString(1, user.getEmail());
+      statement.setString(2, encriptar(user.getSenha()));
+      ResultSet resultado = statement.executeQuery();
+      if (resultado.next()) {
+        user.setIdUsuario(resultado.getInt("id_usuario"));
+        user.setNomeUsuario(resultado.getString("nome_usuario"));
+        user.setEmail(resultado.getString("email"));
+        user.setSenha(resultado.getString("senha"));
+        user.setEditor(resultado.getBoolean("editor"));
+
+        this.connection.setReadOnly(!user.isEditor());
         return true;
       }
       return false;
-    } catch (SQLException e) {
-      Logger erro = Logger.getLogger("erroSQl");
-      erro.log(Level.SEVERE, "excecao levantada:", e);
-      return false;
+    } catch (Exception e) {
+      throw new SQLException("falha validando usuario", e);
+    } finally {
+      statement.close();
     }
   }
 
-  public boolean update(User obj) throws NoSuchAlgorithmException {
+  public boolean update(User user) throws SQLException {
     String sql = "UPDATE usuario SET nome_usuario=?, email=?, senha=?, editor=? WHERE id_usuario=?";
+    PreparedStatement stmt = connection.prepareStatement(sql);
+
     try {
-      PreparedStatement stmt = connection.prepareStatement(sql);
-      stmt.setString(1, obj.getNomeUsuario());
-      stmt.setString(2, obj.getEmail());
-      stmt.setString(3, encriptar(obj.getSenha()));
-      stmt.setBoolean(4, obj.isEditor());
-      stmt.setInt(5, obj.getIdUsuario());
+      stmt.setString(1, user.getNomeUsuario());
+      stmt.setString(2, user.getEmail());
+      stmt.setString(3, encriptar(user.getSenha()));
+      stmt.setBoolean(4, user.isEditor());
+      stmt.setInt(5, user.getIdUsuario());
 
       int numModificacoes = stmt.executeUpdate();
       return numModificacoes > 0;
 
-    } catch (SQLException e) {
-      Logger erro = Logger.getLogger("erroSQl");
-      erro.log(Level.SEVERE, "excecao levantada:", e);
-      return false;
+    } catch (Exception e) {
+      logException(e);
+      throw new SQLException("falha atualizando usuario", e);
     }
   }
 
-  public boolean deletar(User obj) {
+  public boolean deletar(User user) throws SQLException {
     String sql = "DELETE FROM usuario WHERE id_usuario=?";
+    PreparedStatement statement = connection.prepareStatement(sql);
+
     try {
-      PreparedStatement stmt = connection.prepareStatement(sql);
-      stmt.setInt(1, obj.getIdUsuario());
-      int numRemocoes = stmt.executeUpdate();
+      statement.setInt(1, user.getIdUsuario());
+      int numRemocoes = statement.executeUpdate();
       return numRemocoes > 0;
-    } catch (SQLException e) {
-      Logger erro = Logger.getLogger("erroSQl");
-      erro.log(Level.SEVERE, "excecao levantada:", e);
-      return false;
+    } catch (Exception e) {
+      logException(e);
+      throw new SQLException("falha deletando usuario", e);
+    } finally {
+      statement.close();
     }
   }
 }
