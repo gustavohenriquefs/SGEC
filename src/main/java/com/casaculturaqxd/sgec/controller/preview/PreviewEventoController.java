2e4643392e842d70c71181a61196855d1926def2
Robson <robsonad07@gmail.com>
2023-11-29 03:19:38 -0300

    Preview com imagem

--- src/main/java/com/casaculturaqxd/sgec/controller/preview/PreviewEventoController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/preview/PreviewEventoController.java
@@ -1,12 +1,17 @@
 package com.casaculturaqxd.sgec.controller.preview;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
 import com.casaculturaqxd.sgec.App;
 import com.casaculturaqxd.sgec.DAO.EventoDAO;
+import com.casaculturaqxd.sgec.DAO.ServiceFileDAO;
 import com.casaculturaqxd.sgec.controller.VisualizarEventoController;
 import com.casaculturaqxd.sgec.jdbc.DatabasePostgres;
 import com.casaculturaqxd.sgec.models.Evento;
@@ -17,20 +22,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class PreviewEventoController {
     private EventoDAO dao = new EventoDAO();
     private DatabasePostgres db = DatabasePostgres.getInstance("URL", "USER_NAME", "PASSWORD");
+    private ServiceFileDAO serviceFileDAO;
     private Evento evento;
     @FXML
     Label dataHora, titulo;
     @FXML
     Button detalhes;
+    @FXML
+    ImageView imagem;
+
+    File file = null;
 
     public void initialize() {
         dao.setConnection(db.getConnection());
+        serviceFileDAO = new ServiceFileDAO(db.getConnection());
     }
 
     public void setEvento(Evento evento) {
@@ -47,11 +60,26 @@ public class PreviewEventoController {
             this.dataHora.setText(evento.getDataInicial().toString() + "\t" + evento.getHorario());
             this.titulo.setText(evento.getNome());
         }
+        if(evento.getImagemCapa() != null){
+            InputStream fileAsStream;
+            try {
+                file = serviceFileDAO.getContent(evento.getImagemCapa());
+                fileAsStream = new FileInputStream(file);
+                this.imagem.setImage(new Image(fileAsStream));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @FXML
     public void verDetalhes(ActionEvent event) throws IOException, SQLException {
         try {
+            System.out.println("Entre!!!!");
             URL url = App.class.getResource("view/visualizarEvento.fxml");
             FXMLLoader loaderVisualizacao = new FXMLLoader(url);
             Parent objVisualizacao = loaderVisualizacao.load();
