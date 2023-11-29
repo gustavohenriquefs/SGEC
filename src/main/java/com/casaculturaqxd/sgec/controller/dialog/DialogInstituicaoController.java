e123517a7d70b5a10122bd1253b032e0150f2e33
Robson <robsonad07@gmail.com>
2023-11-28 22:00:12 -0300

    mudanças solicitadas feitas

--- src/main/java/com/casaculturaqxd/sgec/controller/dialog/DialogInstituicaoController.java
+++ src/main/java/com/casaculturaqxd/sgec/controller/dialog/DialogInstituicaoController.java
@@ -49,42 +49,14 @@ public class DialogInstituicaoController {
   private Instituicao instituicao = null;
 
   AutoCompletionBinding<String> binding;
+  ArrayList<String> listaNomes;
 
   public void initialize() throws IOException {
-    ArrayList<String> listaNomes;
-    try {
-      listaNomes = instituicaoDAO.listarInstituicoes();
-    } catch (SQLException e) {
-      listaNomes = new ArrayList<>();
-    }
-    
+    carregaListaNomes();
     binding = TextFields.bindAutoCompletion(nomeInstituicao, listaNomes);
-    binding.setOnAutoCompleted(autoCompletionEvent -> {
-      String selected = autoCompletionEvent.getCompletion();
-      try {
-        instituicao = instituicaoDAO.getInstituicao(selected).get();
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
-      if(instituicao.getImagemCapa() != null){
-          InputStream fileAsStream;
-          try {
-              file = serviceFileDAO.getContent(instituicao.getImagemCapa());
-              fileAsStream = new FileInputStream(file);
-              this.imagem.setImage(new Image(fileAsStream));
-          } catch (FileNotFoundException e) {
-              e.printStackTrace();
-          } catch (NullPointerException e) {
-              e.printStackTrace();
-          } catch (SQLException e) {
-              e.printStackTrace();
-          }
-        }
-      nomeInstituicao.setEditable(false);
-    });
-    nomeInstituicao.setOnMouseClicked(mousePressed -> {
-      nomeInstituicao.setEditable(true);
-    });
+    bindingSetOnAutoCompleted();
+    nomeInstituicaoSetOnMouseClicked();
+
   }
 
   public Instituicao obterInstituicao(){
@@ -117,12 +89,24 @@ public class DialogInstituicaoController {
     return temp;
   }
 
+  public void carregaListaNomes(){
+    try {
+      listaNomes = instituicaoDAO.listarInstituicoes();
+    } catch (SQLException e) {
+      listaNomes = new ArrayList<>();
+    }
+  }
+
   public boolean cadastrarInstituicao() {
     if(nomeInstituicao.getText() != ""){
       ServiceFile serviceFileTemp = file != null ? new ServiceFile(file) : null;
       if(serviceFileTemp != null){
         try {
-          serviceFileDAO.inserirArquivo(serviceFileTemp);
+          if(serviceFileDAO.getArquivo(serviceFileTemp.getFileKey()).isEmpty()){
+            serviceFileDAO.inserirArquivo(serviceFileTemp);
+          } else {
+            serviceFileTemp = serviceFileDAO.getArquivo(serviceFileTemp.getFileKey()).get();
+          }
         } catch (SQLException e) {
           e.printStackTrace();
         }
@@ -131,6 +115,10 @@ public class DialogInstituicaoController {
       valorContribuicao.getText(), serviceFileTemp);
       try {
         instituicaoDAO.inserirInstituicao(instituicao);
+        this.carregaListaNomes();
+        this.binding = TextFields.bindAutoCompletion(nomeInstituicao, listaNomes);
+        bindingSetOnAutoCompleted();
+        nomeInstituicaoSetOnMouseClicked();
         return true;
       } catch (SQLException e) {
         e.printStackTrace();
@@ -158,6 +146,39 @@ public class DialogInstituicaoController {
 
   public File getFile() {
     return file;
+  }
+
+  public void bindingSetOnAutoCompleted(){
+    binding.setOnAutoCompleted(autoCompletionEvent -> {
+      String selected = autoCompletionEvent.getCompletion();
+      try {
+        System.out.println(selected);
+        instituicao = instituicaoDAO.getInstituicao(selected).get();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+      if(instituicao.getImagemCapa() != null){
+          InputStream fileAsStream;
+          try {
+              file = serviceFileDAO.getContent(instituicao.getImagemCapa());
+              fileAsStream = new FileInputStream(file);
+              this.imagem.setImage(new Image(fileAsStream));
+          } catch (FileNotFoundException e) {
+              e.printStackTrace();
+          } catch (NullPointerException e) {
+              e.printStackTrace();
+          } catch (SQLException e) {
+              e.printStackTrace();
+          }
+        }
+      nomeInstituicao.setEditable(false);
+    });
+  }
+
+  public void nomeInstituicaoSetOnMouseClicked(){
+    nomeInstituicao.setOnMouseClicked(mousePressed -> {
+      nomeInstituicao.setEditable(true);
+    });
   }
 
 }
